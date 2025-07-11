73ad651718639d0566180d29e9fd4c3116eceb5a
Tyler Kuipers <tyler.kuipers@ama.ab.ca>
2025-07-11 13:20:32 -0600

    Fix the tests

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -2,6 +2,11 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
-    environment: 'nuxt'
+    environment: 'nuxt',
+    environmentOptions: {
+      nuxt: {
+        domEnvironment: 'happy-dom'
+      }
+    }
   }
-}) 
+})
